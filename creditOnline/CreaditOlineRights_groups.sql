8ed9bf71b1ea0c2e76bd700dfea2f517fc4c041e
Jakub Ujwary <jakub.ujwary@gmail.com>
2023-08-24 16:20:25 +0200

    remove unnecessary files

--- creditOnline/CreaditOlineRights_groups.sql
+++
@@ -1,9 +0,0 @@
-create table `groups`
-(
-    id   int auto_increment
-        primary key,
-    name varchar(45) null
-);
-
-INSERT INTO CreaditOlineRights.`groups` (id, name) VALUES (5, 'group1');
-INSERT INTO CreaditOlineRights.`groups` (id, name) VALUES (6, 'group1');
--- creditOnline/CreaditOlineRights_modules.sql
+++
@@ -1,9 +0,0 @@
-create table modules
-(
-    id   int auto_increment
-        primary key,
-    name varchar(45) null
-);
-
-INSERT INTO CreaditOlineRights.modules (id, name) VALUES (1, 'module1');
-INSERT INTO CreaditOlineRights.modules (id, name) VALUES (7, 'module2');
